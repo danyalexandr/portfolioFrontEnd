09064131e6134f8b2a3a57a3de024c640add2116
dany <danyalexandr@gmail.com>
2022-08-27 14:10:15 -0300

    frontEnd 2do commit

--- portfolioFrontEnd/src/app/app.module.ts
+++ portfolioFrontEnd/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProyectsComponent,
+    SkillsComponent,
+    EducationComponent,
+    ExperienceComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
