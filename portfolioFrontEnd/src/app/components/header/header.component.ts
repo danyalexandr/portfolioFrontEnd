48a12f729ba865404f4a9719679fcb1685f4a469
dany <danyalexandr@gmail.com>
2022-09-30 01:01:59 -0300

    trabajando en un header dinamico

--- portfolioFrontEnd/src/app/components/header/header.component.ts
+++ portfolioFrontEnd/src/app/components/header/header.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  aboutMe = 'About Me';
+  nombre = 'Dany Gonzalez';
+  puesto = 'Full Stack Developer Jr';
+  p2 = 'En esta sección se encuentra tu foto de perfil, nombre y titulo que tienes Full Stack Developer Jr; En esta sección debes poner información profesional resumida sobre vos.'
+
   constructor() { }
 
   ngOnInit(): void {
